68c61828e801e43f319e61b16c48dfa0c0dfc0c7
dnpnghia <idnpnlol@gmail.com>
2021-07-27 00:28:31 +0700

    aaa

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 var mongoose = require('mongoose');
 const serverless = require('serverless-http');
 // dotenv
@@ -31,7 +32,7 @@ const router = express.Router();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
